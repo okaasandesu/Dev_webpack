7344356dbe59fe5fc5e4a44a75fb553ca0b230a4
okaasandesu <b7070@oic.jp>
2021-02-08 23:33:02 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// BootstrapのJavaScript側の機能を読み込む
+import "bootstrap";
+
+// スタイルシートを読み込む
+import "./index.scss";
+
+
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: `./src/index.js`,
+
+  // ファイルの出力設定
+  output: {
+    // 出力ファイル名
+    path: path.join(__dirname, "public/javascripts"),
+    filename: "bundle.js"
+  },
+
+  resolve: {
+    fallback: { "buffer": false,
+                "zlib": false,
+                "querystring": false,
+                "path": false,
+                "crypto": require.resolve("crypto-browserify"),
+                "stream": false,
+                "http": false,
+                "url": false,
+                "buffer": false,
+                "util": false,
+                "fs": false,
+                "net": false,
+
+    }
+
+  },
+
+  module: {
+    rules: [
+      {
+        // 対象となるファイルの拡張子(scss)
+        test: /\.scss$/,
+        // Sassファイルの読み込みとコンパイル
+        use: [
+          // CSSファイルを書き出すオプションを有効にする
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          // CSSをバンドルするための機能
+          {
+            loader: "css-loader",
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込まない
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: true,
+              // Sass+PostCSSの場合は2を指定
+              importLoaders: 2,
+            },
+          },
+          // PostCSSのための設定
+          {
+            loader: "postcss-loader",
+            options: {
+              // PostCSS側でもソースマップを有効にする
+              sourceMap: true,
+              postcssOptions: {
+                // ベンダープレフィックスを自動付与する
+                plugins: ["autoprefixer"],
+              },
+            },
+          },
+          // Sassをバンドルするための機能
+          {
+            loader: "sass-loader",
+            options: {
+              // ソースマップの利用有無
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  // source-map方式でないと、CSSの元ソースが追跡できないため
+  devtool: "source-map",
+
+  externals: {
+    express: 'express',
+  },
+};
